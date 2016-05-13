becf4f25ee27b5f6f2599886247240633f73ef15
zachno <zsnokirby@gmail.com>
2016-05-13 16:04:28 -0400

    Updated about page and licenses

--- about.cpp
+++ about.cpp
@@ -3,14 +3,14 @@
 AboutWindow::AboutWindow(QWidget *parent) : QWidget(parent)
 {
     //Create textbox in window
-    this->setFixedSize(250,100);
+    this->setFixedSize(250,140);
     this->setWindowTitle(tr("ASM: About Window"));
     aboutBox = new QLabel;
     aboutBox->setParent(this);
     aboutBox->move(0,60);
     aboutBox->setFixedWidth(250);
     aboutBox->setAlignment(Qt::AlignCenter);
-    aboutBox->setText("Created by ZSNO\nSilk Icon Pack made by Mark James\nUses zlib + QuaZip");
+    aboutBox->setText("Created by ZSNO\nSilk Icon Pack made by Mark James\nUses zlib + QuaZip\nzlib uses the zlib license.\nQuaZip uses LGPL 2 or later\nThis application uses LGPL 3");
     image = new QPixmap;
     image->load(":/Logo.png");
     aboutImage = new QLabel;
